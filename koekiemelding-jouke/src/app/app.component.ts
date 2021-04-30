04aad0f054fe7c55c2b4d420f081bd49ed90f5d5
Jouke de Vries <joukev87@gmail.com>
2021-04-30 16:32:08 +0200

    Added basic animations for intro

---
+++ koekiemelding-jouke/src/app/app.component.ts
@@ -0,0 +1,39 @@
+import { Component, OnInit } from '@angular/core';
+import gsap from "gsap";
+import { Power0, Back, TimelineLite } from "gsap";
+import { TextPlugin } from "gsap/TextPlugin";
+
+gsap.registerPlugin(TextPlugin);
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent implements OnInit {
+  title = 'koekiemelding-jouke';
+
+  ngOnInit() {
+    let intro = new TimelineLite();
+    intro.to('#yellow-rectangle', 0.2, { scaleY: 1, delay: 1, ease: Power0.easeOut })
+    .from('#cookieplate', 0.6, { x: -1500, ease: Back.easeOut  })
+      .to('#text', { duration:0.5, opacity: 1, yoyo: true,ease:Power0.easeNone,repeat:4})
+      .set("#text", {delay: 0.4, text: {value: "3..."}, opacity:1})
+      .set("#text", {delay: 0.8, text: {value: "2..."}})
+      .set("#text", {delay: 0.8, text: {value: "1..."}})
+      .set('#text', {delay: 0.8, text: {value: "START"}, opacity: 1})
+      .add(
+        gsap.timeline({repeat:2})
+          .set('#text', {delay: 0.2, opacity: 0})
+          .set('#text', {delay: 0.2, opacity: 1})
+      )
+      .set('#text', {delay: 0.2, opacity: 0})
+      .from('#lefthand', 0.4, { x: -1500, ease: Power0.easeOut  })
+      .set('#lefthand',{delay: 0.08, attr:{src:"..\\assets\\left_hand_2.png"}})
+      .set('#lefthand',{delay: 0.08, attr:{src:"..\\assets\\left_hand_3.png"}})
+
+    // Hand incoming
+
+
+  }
+}
---
+++ koekiemelding-jouke/src/app/app.module.ts
@@ -0,0 +1,20 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { HandsComponent } from './hands/hands.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    HandsComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
