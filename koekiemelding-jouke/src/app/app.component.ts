72cf392796f609cb56782a9d6329379daf127108
Jouke de Vries <joukev87@gmail.com>
2021-05-03 10:11:30 +0200

    Added right hand graphics

--- koekiemelding-jouke/src/app/app.component.ts
+++ koekiemelding-jouke/src/app/app.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import gsap from "gsap";
 import { Power0, Back, TimelineLite } from "gsap";
 import { TextPlugin } from "gsap/TextPlugin";
+import {Howl, Howler} from 'howler';
 
 gsap.registerPlugin(TextPlugin);
 
@@ -14,7 +15,13 @@ export class AppComponent implements OnInit {
   title = 'koekiemelding-jouke';
 
   ngOnInit() {
+
+    let intro_tune = new Howl({
+      src: ['../assets/soundeffects/fupicat__winsquare.wav'],
+      autoplay: true,
+    });
     let intro = new TimelineLite();
+    intro_tune.play();
     intro.to('#yellow-rectangle', 0.2, { scaleY: 1, delay: 1, ease: Power0.easeOut })
     .from('#cookieplate', 0.6, { x: -1500, ease: Back.easeOut  })
       .to('#text', { duration:0.5, opacity: 1, yoyo: true,ease:Power0.easeNone,repeat:4})
@@ -32,7 +39,6 @@ export class AppComponent implements OnInit {
       .set('#lefthand',{delay: 0.08, attr:{src:"..\\assets\\left_hand_2.png"}})
       .set('#lefthand',{delay: 0.08, attr:{src:"..\\assets\\left_hand_3.png"}})
 
-    // Hand incoming
 
 
   }
